5db9bdfe03b83ec99f0ce2e1d47f226105ef8bbc
larsvandriel <lars.van.driel@hotmail.com>
2022-04-14 14:11:22 +0200

    Implemented KafkaAccessLayer

---
+++ ProductManagementSystem/pms_kafka_access_layer/KafkaProducer.cs
@@ -0,0 +1,30 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManagementSystem.KafkaAccessLayer
+{
+    public class KafkaProducer
+    {
+        public void SendMessage(string topic, string message)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "kafka:9092"
+            };
+
+            Action<DeliveryReport<Null, string>> handler = r =>
+             Console.Out.WriteLine(!r.Error.IsError
+             ? $"Delivered message to {r.TopicPartitionOffset}"
+             : $"Delivery Error: {r.Error.Reason}");
+
+            using var p = new ProducerBuilder<Null, string>(config).Build();
+            p.Produce(topic, new Message<Null, string> { Value = message }, handler);
+
+            p.Flush(TimeSpan.FromSeconds(10));
+        }
+    }
+}
