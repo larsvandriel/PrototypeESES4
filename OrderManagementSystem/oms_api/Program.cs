4e6849bf3e114be53809c60f6dadb0e77a959f6e
larsvandriel <lars.van.driel@hotmail.com>
2022-04-21 01:59:37 +0200

    Implemented OrderManagementSystem API

--- OrderManagementSystem/oms_api/Program.cs
+++ OrderManagementSystem/oms_api/Program.cs
@@ -1,6 +1,34 @@
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.EntityFrameworkCore;
+using OrderManagementSystem.API.Kafka;
+using OrderManagementSystem.DataAccessLayer;
+using OrderManagementSystem.KafkaAccessLayer;
+using OrderManagementSystem.Logic;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddSingleton<IHostedService, KafkaConsumerAcceptOrder>();
+builder.Services.AddSingleton<IHostedService, KafkaConsumerDeclineOrder>();
+builder.Services.AddSingleton<IOrderManager, OrderManager>();
+builder.Services.AddSingleton<IOrderRepository, OrderRepository>();
+builder.Services.AddSingleton<IOrderEventSender, KafkaAccessLayer>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy",
+        builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
+builder.Services.Configure<IISOptions>(options =>
+{
+
+});
+
+ConfigurationManager config = builder.Configuration;
+var connectionString = config["mssqlconnection:connectionString"];
+builder.Services.AddDbContext<RepositoryContext>(options => options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +43,17 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseStaticFiles();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions()
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseRouting();
+
+app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
 
